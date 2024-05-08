e6582912b3ec407369bc0a42cf12b886b35e2be4
Adão Santos <adaosantosn@outlook.com>
2024-05-07 21:53:51 -0400

    Fixig url name app authors

--- authors/urls.py
+++ authors/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from .views import AuthorCreateListView, AuthorRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path("authors/", AuthorCreateListView.as_view(), name="author-create-list"),
+    path("authors/", AuthorCreateListView.as_view(), name="authors-create-list"),
     path(
         "authors/<int:pk>",
         AuthorRetrieveUpdateDestroyView.as_view(),
-        name="author-detail-view",
+        name="authors-detail-view",
     ),
 ]
