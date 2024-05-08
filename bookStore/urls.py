2d99fbb7f1e83d37812c249a70b0a6e43f575f81
Adão Santos <adaosantosn@outlook.com>
2024-05-08 17:44:13 -0400

    Update details swagger doc

--- bookStore/urls.py
+++ bookStore/urls.py
@@ -25,11 +25,10 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="BookStore API",
         default_version="v1",
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
+        description="Manager your books",
+        contact=openapi.Contact(email="adaosantosn@outlook.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
