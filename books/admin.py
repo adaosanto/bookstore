e21e84cbb799f2a63add2e99812b111bea209745
Adão Santos <adaosantosn@outlook.com>
2024-05-08 21:11:56 -0400

    Conf list_display admin app books

--- books/admin.py
+++ books/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Book
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'language', 'genre', 'publisher', 'publication_year', 'pages')
+
+admin.site.register(Book, BookAdmin)
