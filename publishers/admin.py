b3bc507b26bbeac816e3a0352274a178b31eb89c
Adão Santos <adaosantosn@outlook.com>
2024-05-08 21:13:36 -0400

    Conf list_display admin app publishers

--- publishers/admin.py
+++ publishers/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Publisher
 
-admin.site.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+admin.site.register(Publisher, PublisherAdmin)
