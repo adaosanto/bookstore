2e365cfb8761266a2c7b76aa4e5bc571dae08443
Adão Santos <adaosantosn@outlook.com>
2024-05-08 21:12:28 -0400

    Conf list_display admin app genres

--- genres/admin.py
+++ genres/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Genre
 
-admin.site.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+admin.site.register(Genre, GenreAdmin)
