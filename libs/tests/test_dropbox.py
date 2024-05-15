8a060b9db320b65b82375f7949229d7cdc259b9d
Adão Santos <adaosantosn@outlook.com>
2024-05-15 09:45:31 -0400

    Update libs tests

--- libs/tests/test_dropbox.py
+++ libs/tests/test_dropbox.py
@@ -1,9 +1,13 @@
+import sys
 import unittest
 import unittest.mock
+
 from django.conf import settings
+
 from libs.dropbox.get_token import get_token_by_refresh_token
 
-def mocked_requests_get(*args, **kwargs):
+
+def mocked_requests(*args, **kwargs):
     class MockResponse:
         def __init__(self, json_data, status):
             self.json_data = json_data
@@ -11,10 +15,10 @@ def mocked_requests_get(*args, **kwargs):
 
         def json(self):
             return self.json_data
-        
+
         def raise_for_status(self): ...
-        
-    if kwargs['url'] == settings.DROPBOX_API_URL:
+
+    if kwargs["url"] == settings.DROPBOX_API_URL:
         json_data = {
             "access_token": "sl.xpto..",
             "token_type": "bearer",
@@ -28,11 +32,12 @@ def mocked_requests_get(*args, **kwargs):
 
     return MockResponse(json_data, status)
 
-class TestDropBox:
-    @unittest.mock.patch('libs.dropbox.get_token.requests.post', side_effect=mocked_requests_get)
-    def test_get_token(self, mock_get):
-        import sys
-        sys.argv.append('dbbackup')
-        response = get_token_by_refresh_token()
 
-        assert 'sl.' in response
+class TestDropBox:
+    @unittest.mock.patch(
+        "libs.dropbox.get_token.requests.post", side_effect=mocked_requests
+    )
+    def test_get_token(self, mock_get):
+        sys.argv.append("dbbackup")
+        response = get_token_by_refresh_token()
+        assert response.startswith("sl.")
